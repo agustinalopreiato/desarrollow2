e6664ba28993cb6171d179d3cbe90f4b72d9b8b2
agustinalopreiato <aalopreiato@gmail.com>
2024-04-18 20:15:36 -0300

    java

---
+++ js/main.js
@@ -0,0 +1,9 @@
+
+let nombreProducto = "Aire acondicionado"
+let precioUnitario = 900000
+let cantidadDeseada = prompt ("Ingrese la cantidad de aires acondicionados que desea comprar")
+
+let costoFinal = precioUnitario * cantidadDeseada
+
+
+alert (costoFinal)
