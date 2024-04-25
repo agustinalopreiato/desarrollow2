c411ea6fd3a5fb6585f2e16e2bd5e0d16d1bae5b
agustinalopreiato <aalopreiato@gmail.com>
2024-04-25 19:48:07 -0300

    console log

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ let nombreProducto = "Aire acondicionado"
 let precioUnitario = 900000
 let cantidadDeseada = prompt ("Ingrese la cantidad de aires acondicionados que desea comprar")
 
-let costoFinal = precioUnitario * cantidadDeseada
+let costoFinal = precioUnitario * cantidadDeseada 
 
-alert (costoFinal) 
+
+console.log (costoFinal)
